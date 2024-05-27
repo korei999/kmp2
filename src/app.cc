789dbd9ccbf9c6fa62a2f56a6318ebdf53ba2093
korei999 <ju7t1xe@gmail.com>
2024-05-27 18:00:46 +0300

    comments

--- src/app.cc
+++ src/app.cc
@@ -510,7 +510,7 @@ updateParams:
 
         if (bPaused)
         {
-            /* let input thread awake cnd var */
+            /* let input thread awake cndPause */
             mtxPauseSwitch.unlock();
 
             std::unique_lock lock(mtxPause);
