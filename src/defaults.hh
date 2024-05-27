a3d5dc2c72bdf4c5f8f491c024ecd656fcd245a5
korei999 <ju7t1xe@gmail.com>
2024-05-27 20:41:17 +0300

    refactor

--- src/defaults.hh
+++ src/defaults.hh
@@ -5,24 +5,24 @@
 namespace defaults
 {
 
-constexpr f32 maxVolume = 1.2; /* 1.0 == 100% */
-constexpr f32 minVolume = 0.0;
-constexpr f32 volume = 0.15; /* volume at startup */
+constexpr f32 maxVolume   = 1.2; /* 1.0 == 100% */
+constexpr f32 minVolume   = 0.0;
+constexpr f32 volume      = 0.15; /* volume at startup */
 constexpr f64 volumePower = 3.0; /* affects volume curve aka 'std::pow(volume, volumePower)' */
 
-constexpr int step = 100000; /* pcm relative seek step (for sampleRate=48000 -> "100000 / sampleRate" == skip 2.08 sec each step) */
-constexpr u32 updateRate = 100; /* time (ms) between input polls (affects visualizer updates for now) */
-constexpr u32 timeOut = 5000; /* time (ms) to cancel input */
+constexpr int step        = 100000; /* pcm relative seek step (for sampleRate=48000 -> "100000 / sampleRate" == skip 2.08 sec each step) */
+constexpr u32 updateRate  = 100; /* time (ms) between input polls (affects visualizer updates for now) */
+constexpr u32 timeOut     = 5000; /* time (ms) to cancel input */
 
 constexpr bool bWrapSelection = true; /* jump to first after scrolling past the last element in the list */
 
-constexpr bool bDrawVisualizer = true;
-constexpr u32 visualizerUpdateRate = 50; /* time (ms) between visualizer upates ((unused)) */
-constexpr f32 visualizerScalar = 5.0; /* scale the height of each bar */
-constexpr bool bNormalizeVisualizer = false; /* ((unused)) */
+constexpr bool bDrawVisualizer        = true;
+constexpr u32 visualizerUpdateRate    = 50; /* time (ms) between visualizer upates ((unused)) */
+constexpr f32 visualizerScalar        = 5.0; /* scale the height of each bar */
+constexpr bool bNormalizeVisualizer   = false; /* ((unused)) */
 constexpr wchar_t visualizerSymbol[2] = L":";
 
-constexpr bool bTransparentBg = true;
+constexpr bool bTransparentBg      = true;
 constexpr bool bCustomColorPallete = false; /* use custom colors below, otherwise use default terminal colors */
 constexpr color::rgb black   = 0x000000;
 constexpr color::rgb red     = 0xFF0000;
@@ -34,6 +34,6 @@ constexpr color::rgb cyan    = 0x0000FF;
 constexpr color::rgb white   = 0xFFFFFF;
 
 constexpr color::curses borderColor = color::blue;
-constexpr color::curses mutedColor = color::blue;
+constexpr color::curses mutedColor  = color::blue;
 
 } /* namespace defaults */
