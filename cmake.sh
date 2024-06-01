d77672a26b6c4349b52bdee8795795b93eca045b
korei999 <ju7t1xe@gmail.com>
2024-06-01 16:21:35 +0300

    add cmake helper script

---
+++ cmake.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+set -e
+set -x
+
+release()
+{
+    rm -rf build
+    if cmake -G "Ninja" -S . -B build/ -DCMAKE_BUILD_TYPE=Release
+    then
+        cmake --build build/
+    fi
+}
+
+default()
+{
+    rm -rf build
+    if cmake -G "Ninja" -S . -B build/ -DCMAKE_BUILD_TYPE=RelWithDebInfo
+    then
+        cmake --build build/
+    fi
+}
+
+debug()
+{
+    rm -rf build
+    if CC_LD=mold CXX_LD=mold cmake -G "Ninja" -S . -B build/ -DCMAKE_BUILD_TYPE=Debug
+    then
+        cmake --build build/
+    fi
+}
+
+asan()
+{
+    rm -rf build
+    if CC_LD=mold CXX_LD=mold cmake -G "Ninja" -S . -B build/ -DCMAKE_BUILD_TYPE=Asan
+    then
+        cmake --build build/
+    fi
+}
+
+run()
+{
+    BIN=kmp
+
+    if cmake --build build/
+    then
+        echo ""
+        # ASAN_OPTIONS=detect_leaks=1 LSAN_OPTIONS=suppressions=leaks.txt ./build/$BIN "$@" # 2> /tmp/$BIN-dbg.txt
+        # ASAN_OPTIONS=halt_on_error=0 ./build/$BIN "$@" # 2> /tmp/$BIN-dbg.txt
+        # ./build/$BIN "$@" # 2> /tmp/$BIN-dbg.txt
+        ./build/$BIN "$@" 2> /tmp/$BIN-dbg.txt
+    fi
+}
+
+_install()
+{
+    cmake --install build/
+}
+
+_uninstall()
+{
+    sudo xargs rm < ./build/install_manifest.txt
+}
+
+cd $(dirname $0)
+
+case "$1" in
+    run) run "$@" ;;
+    debug) debug ;;
+    asan) asan ;;
+    release) release ;;
+    install) _install ;;
+    uninstall) _uninstall ;;
+    *) default ;;
+esac
